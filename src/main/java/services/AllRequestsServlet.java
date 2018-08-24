a5933957a2f7820b25bd9ee756e493f99dcedf6e
gs.dzhura <dzhura.gs@minsvyazdnr.ru>
2018-08-24 17:42:25 +0300

    24.08.18

--- src/main/java/services/AllRequestsServlet.java
+++ src/main/java/services/AllRequestsServlet.java
@@ -11,55 +11,56 @@ import java.util.Map;
 
 public class AllRequestsServlet extends HttpServlet {
 
+    @Override
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
-
         Map<String, Object> pageVariables = createPageVariablesMap(request);
-        pageVariables.put("value", "");
-
-        //response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));// HEAR
-        //response.getWriter().println(pageVariables.get("value"));
-        //response.getWriter().println(request.getParameter("key"));
-
-        if (pageVariables.containsKey("value")) {
-            //response.getWriter().println(pageVariables.get("value"));
-            response.getWriter().println(request.getParameter("key"));
+        //pageVariables.put("message", "");
+        /*response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
+        response.getWriter().println(pageVariables.get("key").toString());*/
+        if (pageVariables.containsKey("key")) {
+            response.getWriter().println(pageVariables.get("value"));
         } else {
-            return;
-            //response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
-            //response.getWriter().println("value");
+            response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
         }
-
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-
+        //response.setContentType("text/html;charset=utf-8");
+        //response.setStatus(HttpServletResponse.SC_OK);
     }
 
+/*    @Override
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response) throws ServletException, IOException {
         Map<String, Object> pageVariables = createPageVariablesMap(request);
-
         String message = request.getParameter("message");
-
         response.setContentType("text/html;charset=utf-8");
-
         if (message == null || message.isEmpty()) {
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         } else {
             response.setStatus(HttpServletResponse.SC_OK);
         }
         pageVariables.put("message", message == null ? "" : message);
-
         response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
-    }
+    }*/
 
-    public static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+    public static Map<String, Object> createPageVariablesMap(HttpServletRequest request) { // static was
         Map<String, Object> pageVariables = new HashMap<>();
-        pageVariables.put("method", request.getMethod());
-        pageVariables.put("URL", request.getRequestURL().toString());
-        pageVariables.put("pathInfo", request.getPathInfo());
-        pageVariables.put("sessionId", request.getSession().getId());
-        pageVariables.put("parameters", request.getParameterMap().toString());
-        return pageVariables;
+        // pageVariables.put("URL", request.getRequestURL().toString());
+        // pageVariables.put("pathInfo", request.getPathInfo());
+        // pageVariables.put("sessionId", request.getSession().getId());
+        pageVariables.put("", request.getParameterMap().toString());
+        return pageVariables;//dsa
     }
 }
+
+/*    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String message = req.getParameter("key");
+        if (message == null || message.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
+
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().println(message);
+    }*/
